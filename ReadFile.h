07c8dfc4b58f20fd082ac07751652d96da961910
Anthony Kniazewycz <abkniazewy42@students.tntech.edu>
2015-01-28 16:57:12 -0600

    Comments and Documentation

--- ReadFile.h
+++ ReadFile.h
@@ -5,7 +5,14 @@
 
 #include <fstream>
 using namespace std;
-//Last edit by Anthony
+
+//! Class used to read text from a file
+/*!
+ * The ReadFile class accepts a String through the constructor
+ * The class uses this string to create a ifstream variable
+ * It also initializes the _eof and closed variables
+ * The original String is not affected
+ */
 struct ReadFile
 {
    private:
@@ -14,10 +21,15 @@ struct ReadFile
    bool closed;
    
    public:
+//! The constructor accepts a String to be used as the file name for the created ifstream (The String is not affected)
    ReadFile(const char* file_name);
+//! The destructor
    ~ReadFile();
+//! Returns the bool value true or false dependant on whether the end of file has been reached
    bool eof();
+//! closes the ifstream and returns the bool value true
    void close();
+//! reads one line from the ifstream file and returns that value as a String
    String* readLine();
 };
 
