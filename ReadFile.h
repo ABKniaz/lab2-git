868b1bb75b84a4edc5db5322440305d46c04e59e
Anthony Kniazewycz <abkniazewy42@students.tntech.edu>
2015-01-28 14:13:19 -0600

    changed structs to classes

--- ReadFile.h
+++ ReadFile.h
@@ -8,15 +8,17 @@ using namespace std;
 
 struct ReadFile
 {
+   private:
    ifstream input_file;
    bool _eof;
    bool closed;
+   void close(ReadFile* rf);
+   
+   public:
+   ReadFile(const char* file_name);
+   ~ReadFile();
+   bool eof(ReadFile* rf);
+   String* readLine(ReadFile* rf);
 };
-
-ReadFile* createReadFile(const char* file_name);
-void destroyReadFile(ReadFile* rf);
-String* readLine(ReadFile* rf);
-bool eof(ReadFile* rf);
-void close(ReadFile* rf);
 
 #endif
